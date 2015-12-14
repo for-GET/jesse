813aa4462f8fec501f91163173311e9ac391d78d
Andrei Neculau <andrei.neculau@gmail.com>
2015-12-14 16:59:12 +0100

    reindent

--- src/jesse_cli.erl
+++ src/jesse_cli.erl
@@ -32,103 +32,103 @@
 %%%_* API ======================================================================
 
 main([]) ->
-    main(["--help"]);
+  main(["--help"]);
 main(["-h"]) ->
-    main(["--help"]);
+  main(["--help"]);
 main(["--help"]) ->
-    io:fwrite( "Usage: ~s [--json] path_to_json_schema -- "
-               "path_to_json_instance [path_to_json_instance] ~n"
-             , [escript:script_name()]
-             );
+  io:fwrite( "Usage: ~s [--json] path_to_json_schema -- "
+             "path_to_json_instance [path_to_json_instance] ~n"
+           , [escript:script_name()]
+           );
 main(Options) ->
-    main(Options, [], [], []).
+  main(Options, [], [], []).
 
 main(["--json"|Rest], Options, [], []) ->
-    main(Rest, Options ++ [{json, true}], [], []);
+  main(Rest, Options ++ [{json, true}], [], []);
 
 main(["--"|JsonInstances], Options, Schemata, []) ->
-    run(Options, Schemata, JsonInstances);
+  run(Options, Schemata, JsonInstances);
 main([Schema|Rest], Options, Schemata, []) ->
-    main(Rest, Options, [Schema|Schemata], []).
+  main(Rest, Options, [Schema|Schemata], []).
 
 %%%_* Internal =================================================================
 
 run(_Options, _Schemata, []) ->
-    ok;
+  ok;
 run(Options, [Schema|_] = Schemata, [JsonInstance|JsonInstances]) ->
-    JesseResult = jesse_run(JsonInstance, Schema),
-    Result = case JesseResult of
-                 {ok, _} ->
-                     [ {filename, list_to_binary(JsonInstance)}
-                     , {result, ok}
-                     ];
-                 {error, Errors0} ->
-                     Errors = errors_to_json(Errors0),
-                     [ {filename, list_to_binary(JsonInstance)}
-                     , {result, error}
-                     , {errors, Errors}
-                     ]
-             end,
-    case proplists:get_value(json, Options) of
-        undefined ->
-            io:fwrite("~p\n\n", [Result]);
-        true ->
-            io:fwrite("~s\n\n", [jsx:encode(Result)])
-    end,
-    case JesseResult of
-        {ok, _} ->
-            run(Options, Schemata, JsonInstances);
-        _ ->
-            %% init:stop not setting status code correctly
-            %% init:stop(1)
-            halt(1)
-    end.
+  JesseResult = jesse_run(JsonInstance, Schema),
+  Result = case JesseResult of
+             {ok, _} ->
+               [ {filename, list_to_binary(JsonInstance)}
+               , {result, ok}
+               ];
+             {error, Errors0} ->
+               Errors = errors_to_json(Errors0),
+               [ {filename, list_to_binary(JsonInstance)}
+               , {result, error}
+               , {errors, Errors}
+               ]
+           end,
+  case proplists:get_value(json, Options) of
+    undefined ->
+      io:fwrite("~p\n\n", [Result]);
+    true ->
+      io:fwrite("~s\n\n", [jsx:encode(Result)])
+  end,
+  case JesseResult of
+    {ok, _} ->
+      run(Options, Schemata, JsonInstances);
+    _ ->
+      %% init:stop not setting status code correctly
+      %% init:stop(1)
+      halt(1)
+  end.
 
 jesse_run(JsonInstance, Schema) ->
-    %% Don't use application:ensure_all_started(jesse)
-    %% nor application:ensure_started(_)
-    %% in order to maintain compatibility with R16B01 and lower
-    ok = ensure_started(jsx),
-    ok = ensure_started(jesse),
-    {ok, JsonInstanceBinary} = file:read_file(JsonInstance),
-    {ok, SchemaBinary} = file:read_file(Schema),
-    jesse:validate_with_schema( SchemaBinary
-                              , JsonInstanceBinary
-                              , [{parser_fun, fun jsx:decode/1}]
-                              ).
+  %% Don't use application:ensure_all_started(jesse)
+  %% nor application:ensure_started(_)
+  %% in order to maintain compatibility with R16B01 and lower
+  ok = ensure_started(jsx),
+  ok = ensure_started(jesse),
+  {ok, JsonInstanceBinary} = file:read_file(JsonInstance),
+  {ok, SchemaBinary} = file:read_file(Schema),
+  jesse:validate_with_schema( SchemaBinary
+                            , JsonInstanceBinary
+                            , [{parser_fun, fun jsx:decode/1}]
+                            ).
 
 ensure_started(App) ->
-    case application:start(App) of
-        ok ->
-            ok;
-        {error, {already_started, App}} ->
-            ok
-    end.
+  case application:start(App) of
+    ok ->
+      ok;
+    {error, {already_started, App}} ->
+      ok
+  end.
 
 errors_to_json(Errors) ->
-    lists:map(fun error_to_json/1, Errors).
+  lists:map(fun error_to_json/1, Errors).
 
 error_to_json({schema_invalid, Schema, {Error, _}}) ->
-    [ {error, schema_invalid}
-    , {schema, Schema}
-    , {error, Error}
-    ];
+  [ {error, schema_invalid}
+  , {schema, Schema}
+  , {error, Error}
+  ];
 error_to_json({schema_invalid, Schema, Error}) ->
-    [ {error, schema_invalid}
-    , {schema, Schema}
-    , {error, Error}
-    ];
+  [ {error, schema_invalid}
+  , {schema, Schema}
+  , {error, Error}
+  ];
 error_to_json({data_invalid, Schema, {Error, _}, Data, Path}) ->
-    [ {error, data_invalid}
-    , {schema, Schema}
-    , {error, Error}
-    , {path, Path}
-    , {data, Data}
-    ];
+  [ {error, data_invalid}
+  , {schema, Schema}
+  , {error, Error}
+  , {path, Path}
+  , {data, Data}
+  ];
 error_to_json({data_invalid, Schema, Error, Data, Path}) ->
-    [ {error, data_invalid}
-    , {schema, Schema}
-    , {error, Error}
-    , {path, Path}
-    , {data, Data}
-    ].
+  [ {error, data_invalid}
+  , {schema, Schema}
+  , {error, Error}
+  , {path, Path}
+  , {data, Data}
+  ].
