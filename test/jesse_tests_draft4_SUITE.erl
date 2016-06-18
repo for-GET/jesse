2ba28819e4b9fa53e110a906ec50346e85188556
Andrei Neculau (anu) <andrei.neculau@gmail.com>
2016-06-18 23:12:25 +0200

    fix copyright header

--- test/jesse_tests_draft4_SUITE.erl
+++ test/jesse_tests_draft4_SUITE.erl
@@ -1,5 +1,6 @@
 %%%=============================================================================
-%% Copyright 2014 Klarna AB
+%% Copyright 2014- Klarna AB
+%% Copyright 2015- AUTHORS
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
