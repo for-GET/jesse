4066e186699c61aae7cac6c3ae816c08d67cc1ff
Anton Yabchinskiy <arn@bestmx.net>
2016-11-04 22:51:51 +0300

    check standard 'ip-address' format in draft4

--- src/jesse_validator_draft4.erl
+++ src/jesse_validator_draft4.erl
@@ -963,6 +963,11 @@ check_format(Value, _Format = <<"time">>, State) when is_binary(Value) ->
     true  -> State;
     false -> handle_data_invalid(?wrong_format, Value, State)
   end;
+check_format(Value, _Format = <<"ip-address">>, State) when is_binary(Value) ->
+  case inet:parse_ipv4strict_address(binary_to_list(Value)) of
+    {ok, _IPv4Address} -> State;
+    {error, einval}    -> handle_data_invalid(?wrong_format, Value, State)
+  end;
 check_format(_Value, _Format, State) ->
   State.
 
