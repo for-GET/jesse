a4bdb3df938466dbbb417e9e99ad869cb28760e9
Luis Azedo <luis.azedo@factorlusitano.com>
2017-02-24 14:30:11 +0000

    provide schema id to error

--- src/jesse_state.erl
+++ src/jesse_state.erl
@@ -204,14 +204,14 @@ resolve_ref(State, Reference) ->
       Path = jesse_json_path:parse(Pointer),
       case load_local_schema(State#state.root_schema, Path) of
         ?not_found ->
-          jesse_error:handle_schema_invalid(?schema_invalid, State);
+          jesse_error:handle_schema_invalid(Reference, State);
         Schema ->
           set_current_schema(State, Schema)
       end;
     false ->
       case load_schema(State, BaseURI) of
         ?not_found ->
-          jesse_error:handle_schema_invalid(?schema_invalid, State);
+          jesse_error:handle_schema_invalid(Reference, State);
         RemoteSchema ->
           SchemaVer =
             jesse_json_path:value(?SCHEMA, RemoteSchema, ?default_schema_ver),
@@ -222,7 +222,7 @@ resolve_ref(State, Reference) ->
           Path = jesse_json_path:parse(Pointer),
           case load_local_schema(RemoteSchema, Path) of
             ?not_found ->
-              jesse_error:handle_schema_invalid(?schema_invalid, State);
+              jesse_error:handle_schema_invalid(Reference, State);
             Schema ->
               set_current_schema(NewState, Schema)
           end
