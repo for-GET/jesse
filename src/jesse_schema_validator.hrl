b9cb9bb6d8652a2d9f1486afaa6c2affd4a183b7
Andrei Neculau <andrei.neculau@gmail.com>
2017-03-12 01:15:10 +0100

    new schema_not_found reason for schema_invalid

--- src/jesse_schema_validator.hrl
+++ src/jesse_schema_validator.hrl
@@ -74,18 +74,19 @@
 -define(default_error_handler_fun, fun jesse_error:default_error_handler/3).
 
 %% Constant definitions for schema errors
--define(schema_invalid,              'schema_invalid').
 -define(invalid_dependency,          'invalid_dependency').
+-define(schema_invalid,              'schema_invalid').
+-define(schema_not_found,            'schema_not_found').
 -define(schema_unsupported,          'schema_unsupported').
 -define(wrong_all_of_schema_array,   'wrong_all_of_schema_array').
 -define(wrong_any_of_schema_array,   'wrong_any_of_schema_array').
--define(wrong_one_of_schema_array,   'wrong_one_of_schema_array').
 -define(wrong_max_properties,        'wrong_max_properties').
 -define(wrong_min_properties,        'wrong_min_properties').
 -define(wrong_multiple_of,           'wrong_multiple_of').
+-define(wrong_one_of_schema_array,   'wrong_one_of_schema_array').
 -define(wrong_required_array,        'wrong_required_array').
--define(wrong_type_items,            'wrong_type_items').
 -define(wrong_type_dependency,       'wrong_type_dependency').
+-define(wrong_type_items,            'wrong_type_items').
 -define(wrong_type_specification,    'wrong_type_specification').
 
 %% Constant definitions for data errors
