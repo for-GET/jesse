a40b87212d6bdfeb052111757df20b3f9af5e3fe
Andrei Neculau <andrei.neculau@gmail.com>
2018-02-14 15:50:41 +0100

    style

--- src/jesse_error.erl
+++ src/jesse_error.erl
@@ -62,10 +62,13 @@
                            , ErrorList     :: [error_reason()]
                            , AllowedErrors :: jesse_state:allowed_errors()
                            ) -> [error_reason()] | no_return().
-default_error_handler(Error, ErrorList, AllowedErrors) ->
-  case AllowedErrors > length(ErrorList) orelse AllowedErrors =:= ?infinity of
-    true  -> ErrorList ++ [Error];
-    false -> throw(ErrorList ++ [Error])
+default_error_handler(Error, ErrorList0, AllowedErrors) ->
+  ErrorList = ErrorList0 ++ [Error],
+  case AllowedErrors > length(ErrorList0) orelse AllowedErrors =:= ?infinity of
+    true  ->
+      ErrorList;
+    false ->
+      throw(ErrorList)
   end.
 
 %% @doc Generates a new data error and returns the updated state.
