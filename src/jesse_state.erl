2a0fa894bdd6bea553a7b17e734525956b13b321
Shunichi Shinohara <shino@shiguredo.jp>
2020-03-04 10:34:15 +0900

    Cosmetics

--- src/jesse_state.erl
+++ src/jesse_state.erl
@@ -440,7 +440,6 @@ parse_ref(RefBin) ->
     %% Absolute file:
     {error, {no_default_port, file, Ref}} ->
       {absolute, Ref};
-    %% Relative
     _ ->
       {relative, Ref}
   end.
@@ -448,10 +447,8 @@ parse_ref(RefBin) ->
 parse_ref(RefBin) ->
   Ref = unicode:characters_to_list(RefBin),
   case uri_string:parse(Ref) of
-    %% Absolute
     #{scheme := _} ->
       {absolute, Ref};
-    %% Relative
     _ ->
       {relative, Ref}
   end.
