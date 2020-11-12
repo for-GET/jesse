118042761880d342b30bc9f6b86c447bd0d6fdde
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2020-11-12 11:17:29 +0000

    Adapt to higher-level consumption of `jsx`

--- src/jesse_database.erl
+++ src/jesse_database.erl
@@ -318,7 +318,7 @@ add_file_uri(Key0) ->
   "file://" ++ File = Key,
   {ok, SchemaBin} = file:read_file(File),
   {ok, #file_info{mtime = Mtime}} = file:read_file_info(File),
-  Schema = jsx:decode(SchemaBin),
+  Schema = jsx:decode(SchemaBin, [{return_maps, false}]),
   SchemaInfos = [{Key, Mtime, Schema}],
   ValidationFun = fun jesse_lib:is_json_object/1,
   store_schemas(SchemaInfos, ValidationFun).
@@ -328,7 +328,7 @@ add_http_uri(Key0) ->
   Key = jesse_state:canonical_path(Key0, Key0),
   {ok, Response} = httpc:request(get, {Key, []}, [], [{body_format, binary}]),
   {{_Line, 200, _}, Headers, SchemaBin} = Response,
-  Schema = jsx:decode(SchemaBin),
+  Schema = jsx:decode(SchemaBin, [{return_maps, false}]),
   SchemaInfos = [{Key, get_http_mtime(Headers), Schema}],
   ValidationFun = fun jesse_lib:is_json_object/1,
   store_schemas(SchemaInfos, ValidationFun).
