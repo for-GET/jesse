adba01b07eac7e84e7f3e6c26117b0dd5c7e33e9
Andrei Neculau <andrei.neculau@gmail.com>
2024-07-26 13:40:55 +0200

    Revert "dedupe"
    This reverts commit 1150952d976372e5746ad738a7750597875f1b02.

--- Dockerfile.build.sh
+++ Dockerfile.build.sh
@@ -5,6 +5,7 @@ if [ $(awk -F. '{print $1}' /etc/debian_version) -eq 9 ]; then
     printf 'deb http://archive.debian.org/debian/ stretch main contrib non-free deb http://archive.debian.org/debian-security/ stretch/updates main contrib non-free deb http://archive.debian.org/debian/ stretch-backports main contrib non-free' > /etc/apt/sources.list;
 fi
 
+# manually sync with ./.github/workflows/ci.yml
 apt-get update
 apt-get -y install wget
 apt-get -y install libz-dev libssl-dev libcurl4-gnutls-dev libexpat1-dev gettext cmake gcc
