d0c37e129f560c022138113861cd032a358d99ef
Andrei Neculau <andrei.neculau@gmail.com>
2025-09-28 09:10:24 +0200

    handle OTP 28

--- test/jesse_lib_tests.erl
+++ test/jesse_lib_tests.erl
@@ -28,6 +28,29 @@ re_run_default_test_() ->
                    jesse_lib:re_run(<<"3๓३"/utf8>>, "^\\d+$"))}
    ]}.
 
+-if(defined(OTP_RELEASE) andalso ?OTP_RELEASE >= 28).
+re_run_no_ucp_test_() ->
+  {setup,
+   fun() ->
+     application:load(jesse),
+     application:set_env(jesse, re_options, [unicode])
+   end,
+   fun(_) -> application:unload(jesse) end,
+   [
+    {"Do not support beyond ISO Latin-1 letters in \\w without 'ucp'",
+     ?_assertEqual(nomatch,
+                   jesse_lib:re_run(<<"föø"/utf8>>, "^\\w+$"))},
+    {"Do not support beyond ISO Latin-1 numbers in \\d  without 'ucp'",
+     ?_assertEqual(no match,
+                   jesse_lib:re_run(<<"123"/utf8>>, "^\\d+$"))},
+    {"Do not support beyond ISO Latin-1 letters in \\w without 'ucp'",
+     ?_assertEqual(nomatch,
+                   jesse_lib:re_run(<<"fōô"/utf8>>, "^\\w+$"))},
+    {"Do not support beyond ISO Latin-1 numbers in \\d without 'ucp'",
+     ?_assertEqual(nomatch,
+                   jesse_lib:re_run(<<"3๓३"/utf8>>, "^\\d+$"))}
+   ]}.
+-else.
 re_run_no_ucp_test_() ->
   {setup,
    fun() ->
@@ -49,3 +72,4 @@ re_run_no_ucp_test_() ->
      ?_assertEqual(nomatch,
                    jesse_lib:re_run(<<"3๓३"/utf8>>, "^\\d+$"))}
    ]}.
+-endif.
